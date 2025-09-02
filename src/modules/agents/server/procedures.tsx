336a9dcf8c0994ab00bcf953927501c5560ddc03
Arifcodes11 <arifcodes11@gmail.com>
2025-09-02 18:22:11 +0530

    feat: implement agent update and removal functionality; add confirmation dialog and update agent form; enhance agent detail view with edit capabilities

--- src/modules/agents/server/procedures.tsx
+++ src/modules/agents/server/procedures.tsx
@@ -3,7 +3,7 @@ import { and, desc, count, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import { db } from "@/db";
 import { agents } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
-import { agentsInsertSchema } from "../schemas";
+import { agentsInsertSchema, agentsUpdateSchema } from "../schemas";
 import {
   DEFAULT_PAGE,
   DEFAULT_PAGE_SIZE,
@@ -13,21 +13,60 @@ import {
 import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
+  update: protectedProcedure
+    .input(agentsUpdateSchema)
+    .mutation(async ({ ctx, input }) => {
+      const [updatedAgent] = await db
+        .update(agents)
+        .set(input)
+        .where(
+          and(eq(agents.id, input.id), eq(agents.userId, ctx.auth.user.id))
+        )
+        .returning();
+
+      if (!updatedAgent) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Agent not found",
+        });
+      }
+      return updatedAgent;
+      // You can handle updatedAgent, e.g. return or throw error if not found
+    }),
+
+  remove: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const [removedAgent] = await db
+        .delete(agents)
+        .where(
+          and(eq(agents.id, input.id), eq(agents.userId, ctx.auth.user.id))
+        )
+        .returning();
+
+      if (!removedAgent) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Agent not found",
+        });
+      }
+      return removedAgent;
+    }),
+
   getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
-    .query(async ({ input ,ctx }) => {
+    .query(async ({ input, ctx }) => {
       const [existingAgent] = await db
         .select({
           meetingCount: sql<number>`5`,
           ...getTableColumns(agents),
         })
         .from(agents)
-        .where(and(
-          eq(agents.id, input.id),
-          eq(agents.userId,ctx.auth.user.id),
-        ));
-      if(!existingAgent) {
-        throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found"});
+        .where(
+          and(eq(agents.id, input.id), eq(agents.userId, ctx.auth.user.id))
+        );
+      if (!existingAgent) {
+        throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" });
       }
 
       return existingAgent;
